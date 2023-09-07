4883db1a5b09fa54a959eb1d0ad09e340430ad72
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-07 20:23:20 +0500

    525: add ClassB

---
+++ Project01/ClassB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project01
+{
+    class ClassB
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+    }
+}
